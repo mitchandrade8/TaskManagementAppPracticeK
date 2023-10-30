fd5f576748610e71d16cefaba90ab6c2be3f9011
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-29 21:28:17 -0600

    Initial Commit

---
+++ TaskManagementAppPracticeK/TaskManagementAppPracticeKApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppPracticeKApp.swift
+//  TaskManagementAppPracticeK
+//
+//  Created by Mitch Andrade on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppPracticeKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
