17d4af1aaf8feb984eca60756d59105e47086fbd
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-31 17:48:33 -0600

    --Huge Update: Refactor our Model, renamed and cleaned logic

---
+++ TaskManagementAppPracticeK/View/TasksView.swift
@@ -0,0 +1,18 @@
+//
+//  TasksView.swift
+//  TaskManagementAppPracticeK
+//
+//  Created by Mitch Andrade on 10/31/23.
+//
+
+import SwiftUI
+
+struct TasksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasksView()
+}
