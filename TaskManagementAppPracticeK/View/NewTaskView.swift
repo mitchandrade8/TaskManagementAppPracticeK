913d77a59c035d61bf0b4857f3e422df3dc70326
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-31 16:42:24 -0600

    --Progress: adding UI to NewTask Sheet View

--- TaskManagementAppPracticeK/View/NewTaskView.swift
+++ TaskManagementAppPracticeK/View/NewTaskView.swift
@@ -10,10 +10,68 @@ import SwiftUI
 struct NewTaskView: View {
     
     /// View Properties
-    
+    @Environment(\.dismiss) private var dismiss
+    @State private var taskTitle: String = ""
+    @State private var taskDate: Date = .init()
+    @State private var taskColor: Color = .taskColor1
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 15, content: {
+            Button(action: {
+                dismiss()
+            }, label: {
+                Image(systemName: "xmark.circle.fill")
+                    .font(.title)
+                    .tint(.red)
+            })
+            .hSpacing(.leading)
+            
+            VStack(alignment: .leading, spacing: 8, content: {
+                Text("Task Title")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+                
+                TextField("Go for a Walk!", text: $taskTitle)
+                    .padding(.vertical, 12)
+                    .padding(.horizontal, 15)
+                    .background(.white.shadow(.drop(color: .black.opacity(0.25), radius: 2)), in: .rect(cornerRadius: 10))
+            })
+            .padding(.top, 5)
+            
+            HStack(spacing: 12) {
+                VStack(alignment: .leading, spacing: 8, content: {
+                    Text("Task Date")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                    
+                    DatePicker("", selection: $taskDate)
+                        .datePickerStyle(.compact)
+                        .scaleEffect(0.9, anchor: .leading)
+                })
+                .padding(.top, 5)
+                /// Giving some space for tapping 
+                .padding(.trailing, -15)
+                
+                VStack(alignment: .leading, spacing: 8, content: {
+                    Text("Task Color")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                    
+                    let colors: [Color] = [.taskColor1, .taskColor2, .taskColor3, .taskColor4, .taskColor5]
+                    
+                    HStack(spacing: 0) {
+                        ForEach(colors, id: \.self) { color in
+                            Circle()
+                                .fill(color)
+                                .frame(width: 20, height: 20)
+                                .hSpacing(.center)
+                        }
+                    }
+                })
+                .padding(.top, 5)
+            }
+        })
+        .padding(15)
     }
 }
 
