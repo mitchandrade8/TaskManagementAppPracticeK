4d25ad0d42554318d899bc8a7a08affe5529eca3
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-31 16:25:26 -0600

    --Progress: added NewTaskView with a sheet

---
+++ TaskManagementAppPracticeK/View/NewTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTaskView.swift
+//  TaskManagementAppPracticeK
+//
+//  Created by Mitch Andrade on 10/31/23.
+//
+
+import SwiftUI
+
+struct NewTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTaskView()
+}
