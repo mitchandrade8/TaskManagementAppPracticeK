16edb568dc169c2fcc1285e78c1d87aa5e94a0f3
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-31 12:01:46 -0600

    Progress: added some offsets to line up views in Task View

--- TaskManagementAppPracticeK/View/TaskRowView.swift
+++ TaskManagementAppPracticeK/View/TaskRowView.swift
@@ -18,19 +18,25 @@ struct TaskRowView: View {
                 .frame(width: 10, height: 10)
                 .padding(4)
                 .background(.white.shadow(.drop(color: .black.opacity(0.1), radius: 3)), in: .circle)
+                .offset(y: 20)
             
             VStack(alignment: .leading, spacing: 8) {
                 Text(task.taskTitle)
                     .fontWeight(.semibold)
                     .foregroundStyle(.black)
-                    .strikethrough(task.isCompleted, pattern: .solid, color: .black)
+                    
                 
                 Label(task.creationDate.format("hh:mm a"), systemImage: "clock")
                     .font(.caption)
                     .foregroundStyle(.black)
             }
+            .padding(15)
+            .hSpacing(.leading)
+            .background(task.tint, in: .rect(topLeadingRadius: 15, bottomLeadingRadius: 15))
+            .strikethrough(task.isCompleted, pattern: .solid, color: .black)
+            .offset(y: -8)
         }
-        .hSpacing(.leading)
+        
     }
 }
 
